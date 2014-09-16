7432161976e1068b55bae8246fab77463e909eaf
Jan van Oorschot <janvanoorschot@gmail.com>
2014-09-16 16:08:26 +0200

    upgraded from grails 2.4.2 to grails 2.4.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -47,7 +47,7 @@ grails.project.dependency.resolution = {
 
     plugins {
 
-        build ":tomcat:7.0.54"
+        build ":tomcat:7.0.55"
 
         build(":release:3.0.1",
               ":rest-client-builder:1.0.3") {
